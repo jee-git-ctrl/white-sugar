33b4b0e2ee9fa92e88036a7bdbb480f40487bcfa
Kuan314 <limzhengkuan@hotmail.com>
2023-07-11 20:21:53 +0800

    implement admin page

---
+++ src/pages/admin.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Main = styled.div`
+  background-color: #def;
+`;
+
+const Admin = () => {
+  return (
+    <Main>
+      hello
+    </Main>
+  );
+}
+
+export default Admin;
