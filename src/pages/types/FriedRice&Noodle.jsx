64d880094abb8e8f6affa13118ce66d77ebe93ff
Kuan314 <limzhengkuan@hotmail.com>
2023-07-23 17:01:27 +0800

    changing page name

--- src/pages/types/FriedRice&Noodle.jsx
+++ src/pages/types/FriedRice&Noodle.jsx
@@ -7,7 +7,7 @@ export default function FriedRice_Noodle() {
 
   return (
     <Layout>
-      <ShowingFoods Name={"炒饭 / 面 Fried Rice / Noodle"} Foods={friedRice_Noodle} />
+      <ShowingFoods Name={"炒饭 & 面 Fried Rice & Noodle"} Foods={friedRice_Noodle} />
     </Layout>
   )
 }
