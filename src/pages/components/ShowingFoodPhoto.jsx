3207d8e2dafe07acc0de44a72aeac374a1cd84d9
Kuan314 <limzhengkuan@hotmail.com>
2023-07-30 19:21:04 +0800

    try for improve speed

--- src/pages/components/ShowingFoodPhoto.jsx
+++ src/pages/components/ShowingFoodPhoto.jsx
@@ -1,27 +1,56 @@
-import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
+import React, { useMemo } from "react";
 import styled from "styled-components";
 
-const ImageBox = styled.img`
-  border-radius: 40px;
-  height: 10rem;
-  margin: 2px 10%;
+// const ImageBox = styled.img`
+//   border-radius: 40px;
+//   height: 10rem;
+//   margin: 2px 10%;
 
-  @media only screen and (min-width: 768px) {
-    border-radius: 20px;
-  }
+//   @media only screen and (min-width: 768px) {
+//     border-radius: 20px;
+//   }
+// `;
+const ImageBox = styled.div`
+  margin: 2px 0;
 `;
 
 const ShowingFoodPhoto = ({ englishName, imageName }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      allFile {
+        nodes {
+          name
+          childImageSharp {
+            gatsbyImageData(width: 150)
+          }
+        }
+      }
+    }
+  `);
 
-  try {
-    const image = require(`../img/${englishName}/${imageName}.jpg`);
-    return (
-      <ImageBox src={image.default} />
-    )
-  }
-  catch {
-    return null;
-  }
+  const match = useMemo(() => 
+    data.allFile.nodes.find(({ name }) => imageName === name)
+  , [data, imageName]);
+
+  if (!match) return null;
+
+  return (
+    <ImageBox>
+      <GatsbyImage image={match.childImageSharp.gatsbyImageData} />
+    </ImageBox>
+  )
+
+  // try {
+  //   const image = require(`../img/${englishName}/${imageName}.jpg`);
+  //   return (
+  //     <ImageBox src={image.default} />
+  //   )
+  // }
+  // catch {
+  //   return null;
+  // }
 
 }
 
